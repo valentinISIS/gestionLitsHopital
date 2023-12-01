9a1661d311c403322c921df64f3c3df2747e8732
Valentin Laptop <valentin.beaumelle@gmail.com>
2023-12-01 09:20:47 +0100

    Commit Main

---
+++ src/Patient.java
@@ -0,0 +1,9 @@
+public class Patient {
+    private String nom;
+    private String noss;
+
+    public Patient(String nom, String noss) {
+        this.nom = nom;
+        this.noss = noss;
+    }
+}
